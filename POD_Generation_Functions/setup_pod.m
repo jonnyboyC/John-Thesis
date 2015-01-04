02290fecf20f35bdb0f7cd9b95988325ee91eaf7
John Chabot <chabotja@miamioh.edu>
2015-01-04 15:58:36 -0500

    update setup files

--- POD_Generation_Functions/setup_pod.m
+++ POD_Generation_Functions/setup_pod.m
@@ -1,11 +1,23 @@
-problem_base.num_images = 2000;
-problem_base.load_raw = false;
-problem_base.save_pod = true;
-problem_base.direct = 'E:\thesis\PIVData\Baseline';
-problem_base.save_figures = {'fig', 'jpg'};
+problem_base1.num_images = 2000;
+problem_base1.load_raw = false;
+problem_base1.save_pod = true;
+problem_base1.direct = 'E:\thesis\PIVData\Baseline';
+problem_base1.save_figures = {'fig', 'jpg'};
 
-problem_force.num_images = 2000;
-problem_force.load_raw = false;
-problem_force.save_pod = true;
-problem_force.direct = 'E:\thesis\PIVData\Forced';
-problem_force.save_figures = {'fig', 'jpg'};
+problem_base2.num_images = 2000;
+problem_base2.load_raw = false;
+problem_base2.save_pod = true;
+problem_base2.direct = 'D:\shear layer\PIVData\Baseline1';
+problem_base2.save_figures = {'fig', 'jpg'};
+
+problem_force1.num_images = 2000;
+problem_force1.load_raw = false;
+problem_force1.save_pod = true;
+problem_force1.direct = 'E:\thesis\PIVData\Forced';
+problem_force1.save_figures = {'fig', 'jpg'};
+
+problem_force2.num_images = 2000;
+problem_force2.load_raw = false;
+problem_force2.save_pod = true;
+problem_force2.direct = 'D:\shear layer\PIVData\Baseline1';
+problem_force2.save_figures = {'fig', 'jpg'};
