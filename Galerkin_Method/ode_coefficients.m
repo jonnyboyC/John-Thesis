d1b860572ae9d733848f7c30a5f1ced3c86dea26
John Chabot <chabotja@miamioh.edu>
2015-04-02 20:18:00 -0400

    REVISION 100 woo!

--- Galerkin_Method/ode_coefficients.m
+++ Galerkin_Method/ode_coefficients.m
@@ -1,4 +1,4 @@
-function mod_coeff = ode_coefficients(num_modes, fcuh)
+function mod_coeff = ode_coefficients(num_modes, Gal_coeff)
 % Creating linear row of coefficients for eachs modes linear and quadratic
 % terms
 % ODE_COEFFICENTS(NUM_MODES, FCUH) created a row vector of coefficients for
@@ -8,7 +8,7 @@ function mod_coeff = ode_coefficients(num_modes, fcuh)
 mod_coeff = zeros(num_modes, num_modes + (num_modes)*(num_modes+1)/2);
 
 % copy linear terms
-mod_coeff(:,1:num_modes+1) = fcuh(:,1:num_modes + 1);
+mod_coeff(:,1:num_modes) = Gal_coeff(:,1:num_modes);
 
 % Create offset for quadratic terms
 idx = num_modes + 1;
@@ -20,13 +20,13 @@ mat_size = [num_modes num_modes];
 % Add terms to the mod_coeff array, sum terms mirrored across the diagonal
 for i = 1:num_modes;
     for j = i:num_modes;
-        idx = idx + 1;
         if i == j
-            mod_coeff(:,idx) = fcuh(:,offset + sub2ind(mat_size, j, i));
+            mod_coeff(:,idx) = Gal_coeff(:,offset + sub2ind(mat_size, j, i));
         else
-            mod_coeff(:,idx) = fcuh(:,offset + sub2ind(mat_size, j, i)) ...
-                             + fcuh(:,offset + sub2ind(mat_size, i, j));
-         end
+            mod_coeff(:,idx) = Gal_coeff(:,offset + sub2ind(mat_size, j, i)) ...
+                             + Gal_coeff(:,offset + sub2ind(mat_size, i, j));
+        end
+        idx = idx + 1;
     end
 end
 end
