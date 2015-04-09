b555dd0dac713226048307517a58715ed058a6d6
John Chabot <chabotja@miamioh.edu>
2015-04-09 17:59:39 -0400

    derp

--- Plotting functions/plot_flow.m
+++ Plotting functions/plot_flow.m
@@ -1,40 +1,51 @@
-function [handle, cax] = plot_flow(data, h, format)
+function [h_surf, h_quiver, cax] = plot_flow(data, h_s, h_q)
 
-if ~isfield(data, 'pod') || all(isfield(data, {'u', 'v'}))
-    data.pod = sqrt(u.^2 + v.^2);
-else
-    error('Must provide structure DATA with either u and v fields or pod field');
+if ~isfield(data, 'pod') 
+    if all(isfield(data, {'u', 'v'}))
+        data.pod = sqrt(data.u.^2 + data.v.^2);
+    else
+        error('Must provide structure DATA with either u and v fields or pod field');
+    end
 end
 if any(~isfield(data, {'x', 'y'}))
     error('Must provided structure DATA with both x and y fields');
 end
 
+if nargin == 1
+    [h_s, ax] = Plottec2(data);
+else
+    h_s = Plottec2(data, h_s);
+end
 
-[h, ax] = Plottec2(data, h);
-
-
-spacing_x = ceil(size(x, 1)/50);
-spacing_y = ceil(size(x, 2)/50);
+spacing_x = ceil(size(data.x, 1)/50);
+spacing_y = ceil(size(data.x, 2)/50);
 
 short_x = data.x(1:spacing_x:end, 1:spacing_y:end);
 short_y = data.y(1:spacing_x:end, 1:spacing_y:end);
-short_u = data.u(1:spacing_x:end, 1:spacing_y:end, image);
-short_v = data.v(1:spacing_x:end, 1:spacing_y:end, image);
+short_u = data.u(1:spacing_x:end, 1:spacing_y:end);
+short_v = data.v(1:spacing_x:end, 1:spacing_y:end);
 
-hold(ax, 'on')
-quiver(ax, short_x, short_y, short_u, short_v, 'color', [0 0 0]);
-hold(ax, 'off')
+if nargin == 1 
+    hold(ax, 'on')
+    h_q = quiver(ax, short_x, short_y, short_u, short_v, 'color', [0 0 0]);
+    hold(ax, 'off')
+else
+    h_q.UData = short_u;
+    h_q.VData = short_v;
+end
 
-if nargin == 3 && format == true
+if isfield(data, 'format') && data.format == true
     ax.Title.String = ['Instanteous Flow Visualisation for Snapshot ' num2str(image)];
     ax.XLabel.String = 'x/D';
     ax.YLabel.String = 'y/D';
 end
 
-if nargout == 1
-    handle = h;
-end
 if nargout == 2
-    handle = h;
+    h_surf = h_s;
+    h_quiver = h_q;
+end
+if nargout == 3
+    h_surf = h_s;
+    h_quiver = h_q;
     cax = ax;
 end
--- Plotting functions/plot_prediction.m
+++ Plotting functions/plot_prediction.m
@@ -1,6 +1,11 @@
 function plot_prediction(pod_u, pod_v, x, y, bnd_idx, modal_amp, t, dimensions, direct, id)
 % Create a movie of the time response of the predicted Galerkin sytem
-plot_points = 1:size(modal_amp,1);
+max_plot = 200;
+if max_plot < size(modal_amp,1);  
+    plot_points = 1:max_plot;
+else
+    plot_points = 1:size(modal_amp,1);
+end
 modes = size(modal_amp,2);
 
 % Fill all plots with blank images to set renderer to opengl
@@ -19,14 +24,8 @@ if ~exist([direct filesep 'Figures' filesep 'Movies' filesep 'modes_' num2str(mo
     mkdir([direct filesep 'Figures' filesep 'Movies' filesep 'modes_' num2str(modes)]);
 end
 
-file_name = [direct filesep 'Figures' filesep 'Movies' filesep 'modes_' num2str(modes) filesep 'Flow_prediction_'];
-file_name = [file_name id];
-
-
-writer = VideoWriter([file_name '_' num2str(t(1)) '_' num2str(t(end)) 's_' num2str(ceil(Hz)) 'Hz.avi']);
-writer.Quality = 100;
-writer.FrameRate = 60;
-open(writer);
+file_name = [direct filesep 'Figures' filesep 'Movies' filesep 'modes_' ...
+             num2str(modes) filesep 'Flow_prediction_' id];
 
 % TODO May need to relook at this to make it more memory efficient
 data_u = zeros(dimensions(1), dimensions(2), size(plot_points,2));
@@ -34,15 +33,11 @@ data_v = zeros(dimensions(1), dimensions(2), size(plot_points,2));
 data_m = zeros(dimensions(1), dimensions(2), size(plot_points,2));
 
 % calculate predicted images
-idx = 1;
 sum_j = 1:modes;
 for i = plot_points;
-    data_u(:,:,idx) =  data_u(:,:,idx)...
-        + reshape(pod_u(:,sum_j)*modal_amp(i,sum_j)',dimensions(1), dimensions(2));
-    data_v(:,:,idx) =  data_v(:,:,idx)...
-        + reshape(pod_v(:,sum_j)*modal_amp(i,sum_j)',dimensions(1), dimensions(2));
-    data_m(:,:,idx) = sqrt(data_u(:,:,idx).^2+data_v(:,:,idx).^2);
-    idx = idx + 1;
+    data_u(:,:,i) = reshape(pod_u(:,sum_j)*modal_amp(i,sum_j)',dimensions(1), dimensions(2));
+    data_v(:,:,i) = reshape(pod_v(:,sum_j)*modal_amp(i,sum_j)',dimensions(1), dimensions(2));
+    data_m(:,:,i) = sqrt(data_u(:,:,i).^2+data_v(:,:,i).^2);
 end
 
 type = {'Flow Visualization'};
@@ -50,12 +45,17 @@ type = {'Flow Visualization'};
 % Determine max values for u v and magnitude
 % Leave a bit left off so useful plots can be produced even with blow up
 [~, idx] = sort(abs(data_m(:)));
-cmax = abs(data_m(idx(0.95*floor(length(idx)))));
-cmin = -cmax;
+cmax = abs(data_m(idx(floor(0.95*length(idx)))));
+cmin = 0;
 
 data_temp.x = x;
 data_temp.y = y;
 data_temp.bnd_idx = bnd_idx;
+
+writer = VideoWriter([file_name '_' num2str(t(1)) '_' num2str(t(end)) 's_' num2str(ceil(Hz)) 'Hz.avi']);
+writer.Quality = 100;
+writer.FrameRate = 60;
+open(writer);
 
 % Plot results, print current image number, and save images to .avi video
 for i = 1:size(plot_points,2)  
@@ -66,7 +66,7 @@ for i = 1:size(plot_points,2)
         data_temp.v = squeeze(data_v(:,:,i));
         if i == 1
             figure(h_parent);
-            [h_child, ax] = Plottec2(data_temp);
+            [h_surf, h_quiver, ax] = plot_flow(data_temp);
             ax.Title = title(type{j}, 'fontname','times new roman','fontsize', 14);
             ax.XLabel = xlabel('x/D', 'fontname','times new roman','fontsize',12);
             ax.YLabel = ylabel('y/D', 'fontname','times new roman','fontsize',12);
@@ -74,7 +74,7 @@ for i = 1:size(plot_points,2)
             ax.CLim = [cmin, cmax];
             colorbar;
         else
-            h_child = Plottec2(data_temp, h_child);
+            [h_surf, h_quiver] = plot_flow(data_temp, h_surf, h_quiver);
         end
     end
     frame = getframe(gcf);
