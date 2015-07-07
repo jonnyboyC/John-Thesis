72e97a0dd85873e3669dcdc33164b3fa50fc9357
John Chabot <chabotja@miamioh.edu>
2015-07-07 12:35:27 -0400

    added cluster script

---
+++ scripts/cluster_cavity1.m
@@ -0,0 +1,10 @@
+load('/home/chabotja/Thesis/PIVData/Cavity/direct1');
+
+setup_pod
+for i = 1:length(direct)
+    for j = 1:3
+        problem_cavity.num_clusters = 8 + (j-1)*2;
+        problem_cavity.direct = direct{i};
+        POD_Gen(problem_cavity);
+    end
+end
