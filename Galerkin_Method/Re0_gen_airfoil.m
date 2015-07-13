a17df680d08e3c588564f0594d923001de49247b
John Chabot <chabotja@miamioh.edu>
2015-07-13 10:21:09 -0400

    more fixing stuff

--- Galerkin_Method/Re0_gen_airfoil.m
+++ Galerkin_Method/Re0_gen_airfoil.m
@@ -2,6 +2,11 @@ function Re0 = Re0_gen_airfoil(direct)
 airfoil_path = [direct, filesep 'Other' filesep 'A2_CpPHF_NS_20100728b.xls'];
 [data, ~] = xlsread(airfoil_path, 'DataSheet');
 test = regexp(direct, '[0-9]*', 'match');
+if isempty(test)
+    test = 1;
+else
+    test = test + 1;
+end
 test = str2double(test);
 Re0 = data(:,2)*1000;
 Re0 = Re0(test);
